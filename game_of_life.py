670847708cca27eddeef70f186902c527dd13f4c
maybelinot <edik.trott@yandex.ru>
2015-06-22 12:47:31 +0200

    continuous playback

--- game_of_life.py
+++ game_of_life.py
@@ -13,25 +13,24 @@ class Game():
                                      [1, 1, 1]], np.uint8)
         self.fig = plt.figure()
         self.size = size
-        self.ims = []
         if inside == "random":
             self.field = np.random.randint(2, size=(size,size))
         elif inside == "empty":
             self.field = np.zeros((size, size))
+        self.im = plt.imshow(self.field, cmap=plt.cm.gray, interpolation='nearest')
 
     def __str__(self):
         pass
 
-    def  play(self, iterations = 20):
-        for _ in range(iterations):
-            conv_field = signal.convolve2d(self.field, self.filter, boundary='symm', mode='same')
-            self.field = np.array(np.logical_or(np.logical_and(self.field == 1, np.logical_and(conv_field > 1, conv_field < 4)),
-                                    np.logical_and(self.field == 0, conv_field == 3))).astype(int)
-            self.ims.append([plt.imshow(self.field, cmap=plt.cm.gray, interpolation='nearest')])
+    def  play(self, *args):
+        conv_field = signal.convolve2d(self.field, self.filter, boundary='symm', mode='same')
+        self.field = np.array(np.logical_or(np.logical_and(self.field == 1, np.logical_and(conv_field > 1, conv_field < 4)),
+                                np.logical_and(self.field == 0, conv_field == 3))).astype(int)
+        self.im.set_array(self.field)
+        return self.im,
 
     def rendering(self, delay = 2):
-        self.ani = animation.ArtistAnimation(self.fig, self.ims, interval=200, blit=True,
-            repeat_delay=1000)
+        self.ani = animation.FuncAnimation(self.fig, self.play, interval=50, blit=True)
         plt.show()
 
 
